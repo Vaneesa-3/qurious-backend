8de48fdab2843de2a1874936adea8ca6d3c37309
Vaneesa Joju <vaneesajojuann@gmail.com>
2025-07-17 12:55:36 +0530

    Removed commented MongoDB URL for security

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-//mongoose.connect('mongodb+srv://vaneesajojuann:vaneesa@cluster0.ugerryu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
 require('dotenv').config();
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('Connected!')).catch((err)=>{
